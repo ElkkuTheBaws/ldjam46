b3e50e411bc65578f0974a91b2d2c47fe9dc4b82
ElkkuTheBaws <58703912+ElkkuTheBaws@users.noreply.github.com>
2020-04-18 15:20:41 +0300

    Pick system

--- pickable.gd
+++ pickable.gd
@@ -2,11 +2,20 @@ extends RigidBody2D
 class_name pickable
 
 var MASS = 500
+var can_pick = false
+var picked = false
 onready var player_node = get_node("../Player/Position2D")
 func _ready():
-	set_physics_process(false)
+	set_physics_process(true)
 	add_to_group("pickable")
 	
 	
 func _physics_process(delta):
-	self.position = player_node.global_position
+	if can_pick == true:
+		if Input.is_action_just_pressed("ui_pick"):
+			picked = true
+	
+	if(picked):
+		print("picked")
+		picked = false
+#	self.position = player_node.global_position
