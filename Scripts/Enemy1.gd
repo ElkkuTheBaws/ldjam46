49f36645fe2897605359600537cbda5a0a7a9d50
ElkkuTheBaws <58703912+ElkkuTheBaws@users.noreply.github.com>
2020-04-18 23:07:17 +0300

    pathfinding not working :(

--- Scripts/Enemy1.gd
+++ Scripts/Enemy1.gd
@@ -3,20 +3,22 @@ extends Enemy
 export var speed = 200.0
 export var min_distance = 300.0
 onready var nav_2d : Navigation2D = get_node("../Navigation2D")	
+onready var pleijeri = get_node("../Player")	
+var path : = PoolVector2Array()
 var tplayer : player
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	set_physics_process(false)
-	return
 
 
 func _physics_process(delta: float) -> void:
-	var direction: = (tplayer.global_position - global_position).normalized()
-	var distance_to_player: = global_position.distance_to(tplayer.global_position)
-	if distance_to_player <= min_distance:
-		collision = move_and_collide(speed * direction * delta)
-	
-	return
+#	var direction: = (tplayer.global_position - global_position).normalized()
+#	var distance_to_player: = global_position.distance_to(tplayer.global_position)
+#	if distance_to_player <= min_distance:
+#		collision = move_and_collide(speed * direction * delta)
+	var move_distance = (speed * delta)
+	calculate_path()
+	move_along_path(move_distance, delta)
 
 
 func _on_PlayerDetector_body_entered(body: Node) -> void:
@@ -24,17 +26,25 @@ func _on_PlayerDetector_body_entered(body: Node) -> void:
 		return
 	tplayer = body
 	set_physics_process(true)
-	return
 
-func make_path():
-	var new_path : = nav_2d.get_simple_path(self.global_position, player.position) #player.pos ei pakolla toimi
-	if new_path.size() == 0:
+func calculate_path():
+	var new_path = nav_2d.get_simple_path(self.global_position, pleijeri.position)
+	path = new_path
+	if path.size() == 0:
 		return
-	set_physics_process(true)
-	
-#func move_along_path(distance : float, path) -> void:
-#	var start_point : = position
-#	for i in range(path.size()):
-#		var distance_to_next : = start_point.distance_to(path[0])
-#		if distance <= distance_to_next and distance >= 0.0:
-#			position = start_point.linear_interpolate()
+
+func move_along_path(distance : float, delta) -> void:
+	print(path.size())
+	var start_point : = position
+	for i in range(path.size()):
+		var distance_to_next : = start_point.distance_to(path[0])
+		if distance <= distance_to_next and distance >= 0.0:
+			move_and_slide((path[0] - global_position).normalized() * delta * speed)
+			break
+		elif distance < 0.0:
+#			position = path[0]
+			set_physics_process(false)
+			break
+		distance -= distance_to_next
+		start_point = path[0]
+		path.remove(0)
