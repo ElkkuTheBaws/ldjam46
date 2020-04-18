04dca193db13fcc8bf7f1e3e1dae8c0139718306
ElkkuTheBaws <58703912+ElkkuTheBaws@users.noreply.github.com>
2020-04-18 21:35:31 +0300

    Starting the pathfinding

--- Scripts/Enemy1.gd
+++ Scripts/Enemy1.gd
@@ -1,9 +1,8 @@
 extends Enemy
 
-var speed = 200.0
-var min_distance = 300.0
-
-
+export var speed = 200.0
+export var min_distance = 300.0
+onready var nav_2d : Navigation2D = get_node("../Navigation2D")	
 var tplayer : player
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -27,4 +26,15 @@ func _on_PlayerDetector_body_entered(body: Node) -> void:
 	set_physics_process(true)
 	return
 
-
+func make_path():
+	var new_path : = nav_2d.get_simple_path(self.global_position, player.position) #player.pos ei pakolla toimi
+	if new_path.size() == 0:
+		return
+	set_physics_process(true)
+	
+#func move_along_path(distance : float, path) -> void:
+#	var start_point : = position
+#	for i in range(path.size()):
+#		var distance_to_next : = start_point.distance_to(path[0])
+#		if distance <= distance_to_next and distance >= 0.0:
+#			position = start_point.linear_interpolate()
