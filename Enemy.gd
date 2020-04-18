99449b446881cbee313056129d63b41ed4df9c94
Nico Kääriäinen <nico.kaariainen0@gmail.com>
2020-04-18 13:56:48 +0300

    Enemy class things

---
+++ Enemy.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+class_name Enemy
+
+var MAX_SPEED = 200
+var ACCELERATION = 2000
+var motion = Vector2.ZERO
+var HP = 2
+
+func _physics_process(delta: float) -> void:
+	return
+
+func lose_hp() -> void:
+	HP -= 1
+	if(HP == 0):
+		queue_free()
+	return
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
