9d71d740ec2c15bb54422bbe83ffa4dbc2e01e21
Nico Kääriäinen <nico.kaariainen0@gmail.com>
2020-04-18 21:58:09 +0300

    Throwing made with tweening

--- Scripts/pickable.gd
+++ Scripts/pickable.gd
@@ -6,12 +6,14 @@ onready var player: = get_node("../Player")
 
 var MASS = 500
 var speed = 200
+var throw_distance = 50
 var can_pick = false
 var picked = false
 var inAir = false
 var throwtime
 var throw_direction = Vector2.RIGHT
 var collision
+var tween : Tween
 
 func _ready():
 	set_physics_process(true)
@@ -28,15 +30,13 @@ func _physics_process(delta):
 		self.position = player_node.global_position + Vector2(0,-5)
 		
 		if Input.is_action_just_pressed("ui_accept"):
-			if player.motion == Vector2.ZERO:
-				throw_object_still(player.current_direction)
-			else:
-				 throw_object(player.motion)
-	if inAir:
-		collision = move_and_collide(throw_direction * speed * delta)
+			throw_object_still(player.current_direction)
+
+	#if inAir:
+		#collision = move_and_collide(throw_direction * speed * delta)
 		
-		if collision: #Tests if object collides with something in air
-			pass
+		#if collision: #Tests if object collides with something in air
+			#pass
 		
 #	self.position = player_node.global_position
 
@@ -45,6 +45,10 @@ func throw_object_still(var direction):
 	throw_direction = direction
 	picked = false
 	
+	tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property(self, "position", global_position, player.throw_destination, 0.5,Tween.TRANS_CIRC,Tween.EASE_OUT)
+	tween.start()	
 	#Creating a timer for the throw distance
 	throwtime = Timer.new()
 	throwtime.one_shot = true
@@ -52,19 +56,19 @@ func throw_object_still(var direction):
 	throwtime.connect("timeout",self,"_on_throwtime_timeout")
 	throwtime.start(0.5)
 	
-func throw_object(var direction: Vector2):
-	inAir = true
-	throw_direction = direction / 200
-	if throw_direction.length() < sqrt(2):
-		throw_direction = direction.normalized()
-	picked = false
-	
-	#Creating a timer for the throw distance
-	throwtime = Timer.new()
-	throwtime.one_shot = true
-	add_child(throwtime)
-	throwtime.connect("timeout",self,"_on_throwtime_timeout")
-	throwtime.start(0.5)
+#func throw_object(var direction: Vector2):
+#	inAir = true
+#	throw_direction = direction / 200
+#	if throw_direction.length() < sqrt(2):
+#		throw_direction = direction.normalized()
+#	picked = false
+#
+#	#Creating a timer for the throw distance
+#	throwtime = Timer.new()
+#	throwtime.one_shot = true
+#	add_child(throwtime)
+#	throwtime.connect("timeout",self,"_on_throwtime_timeout")
+#	throwtime.start(0.5)
 
 func _on_throwtime_timeout():
 	inAir = false
