1e192fb9bbdbd89eb600a8c6c19e4cd02ee7900f
Nico Kääriäinen <nico.kaariainen0@gmail.com>
2020-04-18 19:07:55 +0300

    Throwing getting better

--- Scripts/pickable.gd
+++ Scripts/pickable.gd
@@ -2,10 +2,10 @@ extends KinematicBody2D
 class_name pickable
 
 onready var player_node = get_node("../Player/Position2D")
-onready var player = get_node("../Player")
+onready var player: = get_node("../Player")
 
 var MASS = 500
-var speed = 30000
+var speed = 300
 var can_pick = false
 var picked = false
 var inAir = false
@@ -28,10 +28,10 @@ func _physics_process(delta):
 		self.position = player_node.global_position + Vector2(0,-20)
 		
 		if Input.is_action_just_pressed("ui_accept"):
-			throw_object(player.current_direction)
-	else:
-		self.collision_layer = 1
-		self.collision_mask = 1
+			if player.motion == Vector2.ZERO:
+				throw_object_still(player.current_direction)
+			else:
+				 throw_object(player.motion)
 	if inAir:
 		collision = move_and_collide(throw_direction * speed * delta)
 		
@@ -40,7 +40,7 @@ func _physics_process(delta):
 		
 #	self.position = player_node.global_position
 
-func throw_object(var direction):
+func throw_object_still(var direction):
 	inAir = true
 	throw_direction = direction
 	picked = false
@@ -51,9 +51,24 @@ func throw_object(var direction):
 	add_child(throwtime)
 	throwtime.connect("timeout",self,"_on_throwtime_timeout")
 	throwtime.start(0.5)
+	
+func throw_object(var direction: Vector2):
+	inAir = true
+	throw_direction = direction / 200
+	if throw_direction.length() < sqrt(2):
+		throw_direction = direction.normalized()
+	picked = false
+	
+	#Creating a timer for the throw distance
+	throwtime = Timer.new()
+	throwtime.one_shot = true
+	add_child(throwtime)
+	throwtime.connect("timeout",self,"_on_throwtime_timeout")
+	throwtime.start(0.5)
 
 func _on_throwtime_timeout():
-	print("epic")
 	inAir = false
+	self.collision_layer = 1
+	self.collision_mask = 1
 
 
