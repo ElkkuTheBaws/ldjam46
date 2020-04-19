923446c3358a2093f84e6bbe5870e6d08a60f561
Nico Kääriäinen <nico.kaariainen0@gmail.com>
2020-04-19 16:14:48 +0300

    Throwing progressbar

---
+++ Scripts/ProgressBar.gd
@@ -0,0 +1,30 @@
+extends ProgressBar
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+onready var timer = get_node("Timer")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = false;
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+func _on_Player_throw_length_changed(length) -> void:
+	if(length > 0):
+		visible = true
+		value = length
+		timer.stop()
+		timer.start(1)
+
+
+func _on_Timer_timeout() -> void:
+	visible = false
+	value = 0
