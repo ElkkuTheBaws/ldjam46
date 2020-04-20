6fb6199a2d3b9e4ffd54867105a85a7a9ad6c603
ElkkuTheBaws <58703912+ElkkuTheBaws@users.noreply.github.com>
2020-04-20 21:17:41 +0300

    doors

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -2,7 +2,11 @@ extends KinematicBody2D
 
 
 export var id = 0
-# Declare member variables here. Examples:
+export(int, "House Door Front", "House Door Side") var doors
+var doorup
+var dooractive
+var doordown
+onready var door_sprite = $Sprite
 # var a: int = 2
 # var b: String = "text"
 onready var collisionshape = get_node("CollisionShape2D")
@@ -10,7 +14,17 @@ onready var collisionshape = get_node("CollisionShape2D")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+		match doors:
+			0:
+				doorup = preload("res://Textures/doors/16x16 tile doors_0.png")
+				doordown = preload("res://Textures/doors/16x16 tile doors_2.png")
+				dooractive = preload("res://Textures/doors/16x16 tile doors_1.png")
+				door_sprite.texture = doorup
+			1:
+				doorup = preload("res://Textures/doors/16x16 tile doors_3.png")
+				doordown = preload("res://Textures/doors/16x16 tile doors_5.png")
+				dooractive = preload("res://Textures/doors/16x16 tile doors_4.png")
+				door_sprite.texture = doorup
 
 
 
@@ -25,9 +39,11 @@ func _on_Pressureplate_activated(isActivated, id) -> void:
 func open_door():
 	set_collision_layer_bit(3, false)
 	set_collision_mask_bit(3,false)
-	visible = false
+	door_sprite.texture = doordown
+#	visible = false
 	
 func close_door():
 	set_collision_layer_bit(3, true)
 	set_collision_mask_bit(3,true)
-	visible = true
+	door_sprite.texture = doorup
+#	visible = true
